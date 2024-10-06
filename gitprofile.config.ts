e3cac4acb349b7a5a2b048c153e821088e76227f
hodgesodev <hodgesodev@gmail.com>
2024-10-05 23:17:23 -0400

    Another resume download fix attempt

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -155,9 +155,9 @@ const CONFIG = {
     username: 'hodgesodev', // to hide blog section, keep it empty
     limit: 3, // How many articles to display. Max is 10.
   },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+  // googleAnalytics: {
+  //   id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  // },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
     id: '',
